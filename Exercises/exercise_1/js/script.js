f3d7f34eace335efaccc98a253c09c3d61de79c3
sandamali-iresha <sadamalisadu200@gmail.com>
2023-11-22 20:01:13 +0530

    new repository

---
+++ Exercises/exercise_1/js/script.js
@@ -0,0 +1,17 @@
+alert("Hello frontend developer");
+ 
+function init() {
+    document.getElementById("about").style.color ='purple'
+}
+
+function date_add(){
+    document.getElementById('demo').innerHTML = Date()
+}
+
+function turnOnLight() {
+    document.getElementById('myImage').src='img/download (2).png'
+}
+
+function push2() {
+    document.getElementById('myImage').src='img/download (1).png'
+}
